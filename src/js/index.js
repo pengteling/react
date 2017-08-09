89bfecbb165f246ab23e8c90a68f7ae04760497e
pengteling <pengteling@gmail.com>
2017-08-09 21:13:43 +0800

    jsx

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,12 @@ ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('app')
 );
+var props ={
+  foo:'default'
+};
+var component =(
+  <div {...props}>test</div>);
+console.log(component.props.foo);
+var component2 =(
+  <div {...props} foo="news">test</div>);
+console.log(component2.props.foo);
