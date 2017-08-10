07535eb2f7a708fb882d05b74bc6aebc2f84f501
pengteling <pengteling@gmail.com>
2017-08-10 16:36:50 +0800

    components

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,28 @@
-console.log("text");
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Es5写法
+// var HelloMessage = React.createClass({
+//   render: function() {
+//     return <h1>Hello World！</h1>;
+//   }
+// });
+
+// ES6写法
+// class HelloMessage extends React.Component{
+//   render(){
+//     return <h1>Hello World!</h1>;
+//   }
+// }
+
+//直接写一个函数也可以定义一个组件
+function HelloMessage(props){
+  //props.name = "fy"
+  //let v="fy"
+  return  <h1>Hello {props.name}!</h1>
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <HelloMessage name="world" />,
   document.getElementById('app')
 );
-var props ={
-  foo:'default'
-};
-var component =(
-  <div {...props}>test</div>);
-console.log(component.props.foo);
-var component2 =(
-  <div {...props} foo="news">test</div>);
-console.log(component2.props.foo);
