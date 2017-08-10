985d4e372c06c651f88faf9affd06198a27dddaa
pengteling <pengteling@gmail.com>
2017-08-10 21:30:56 +0800

    all ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// var HelloMessage = React.createClass({
-//   getInitialState:function(){
-//     return{
-//         state1:"state1"
-//     }
-//   },
-//   getDefaultProps:function(){
-//     return{
-//       name:'fy'
-//     }
-//   },
-//   componentDidMount(){
-//     //this.state.state1 = "new state"
-//     this.setState({state1:"new state"});
-//   },
-//   propTypes:{
-//     name: React.PropTypes.string.isRequired,
-//     title: React.PropTypes.string.isRequired
-//   },
-//   render:function() {
-//
-//     return <h1>hello,{this.props.name} {this.props.title} {this.state.state1}</h1>
-//   }
-// })
-
-class HelloMessage extends React.Component{
+class Box extends React.Component{
   constructor(props){
     super(props)
-    this.state= {state1:"state1"}
+    this.state ={state1:"1"}
+    this.handleClick = this.handleClick.bind(this)
   }
-  componentDidMount(){
-    this.setState({state1: "new state"})
+  handleClick(){
+    console.log("handleClick")
+    this.setState({state1: new Date()})
   }
   render(){
-    return <h1>hello,{this.props.name} {this.props.title} {this.state.state1}</h1>
+    return (
+      <div onClick={this.handleClick}><Clock date={this.state.state1}/></div>
+    )
+  }
+}
+class Clock extends React.Component{
+  constructor(props){
+    super(props)
+    this.state= {date:new Date()}
+  }
+  componentDidMount(){
+    console.log("componentDidMount")
+    this.num =0;
+    this.timer = setInterval( () =>{
+      this.num ++;
+      this.setState({date: new Date()})
+    } ,1000)
+
+
+
+    //第二种写法
+    // setInterval(function(){
+    //   this.setState({date: new Date()});
+    // }.bind(this),1000)
+  }
+  componentWillReceiveProps(nextProps){
+    console.log("componentWillReceiveProps")
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("shouldComponentUpdate")
+    return true
+  }
+  componentWillMount(){
+    //组件第一次建立
+    console.log("componentWillMount")
+  }
+  componentWillUpdate(){
+    console.log("componentWillUpdate")
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate")
+    if(this.num>3){
+      //console.log("unmount");
+      //销毁组件
+      //ReactDOM.unmountComponentAtNode(document.getElementById('app'))
+      clearInterval(this.timer);
+    }
+  }
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+    clearInterval(this.timer);
+  }
+
+  render(){
+    console.log("render")
+    return (
+      <div>
+      <h1>hello</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
   }
 }
 
-HelloMessage.defaultProps ={
-  name:"fy"
-}
-HelloMessage.propTypes ={
-  title: React.PropTypes.string.isRequired
-}
+
 
 ReactDOM.render(
-  <HelloMessage name="world" title="test"/>,
+  <Box />,
   document.getElementById('app')
 );
