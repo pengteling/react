ffe6726ac0398d02388a985f0eace9e63a43f464
pengteling <pengteling@gmail.com>
2017-08-11 17:02:55 +0800

    event

--- src/js/index.js
+++ src/js/index.js
@@ -1,81 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// class Box extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state ={state1:(new Date()).toLocaleTimeString()}
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+//   handleClick(event){
+//     console.log(event.target)
+//     console.log(event.clientX)
+//     this.setState({state1: (new Date()).toLocaleTimeString()})
+//   }
+//   render(){
+//     return (
+//       <div onClick={this.handleClick}>{this.state.state1}</div>
+//     )
+//   }
+// }
+
+
 class Box extends React.Component{
   constructor(props){
     super(props)
-    this.state ={state1:"1"}
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick(){
-    console.log("handleClick")
-    this.setState({state1: new Date()})
-  }
-  render(){
-    return (
-      <div onClick={this.handleClick}><Clock date={this.state.state1}/></div>
-    )
-  }
-}
-class Clock extends React.Component{
-  constructor(props){
-    super(props)
-    this.state= {date:new Date()}
-  }
-  componentDidMount(){
-    console.log("componentDidMount")
-    this.num =0;
-    this.timer = setInterval( () =>{
-      this.num ++;
-      this.setState({date: new Date()})
-    } ,1000)
-
-
-
-    //第二种写法
-    // setInterval(function(){
-    //   this.setState({date: new Date()});
-    // }.bind(this),1000)
-  }
-  componentWillReceiveProps(nextProps){
-    console.log("componentWillReceiveProps")
-  }
-  shouldComponentUpdate(nextProps, nextState){
-    console.log("shouldComponentUpdate")
-    return true
-  }
-  componentWillMount(){
-    //组件第一次建立
-    console.log("componentWillMount")
-  }
-  componentWillUpdate(){
-    console.log("componentWillUpdate")
-  }
-  componentDidUpdate(){
-    console.log("componentDidUpdate")
-    if(this.num>3){
-      //console.log("unmount");
-      //销毁组件
-      //ReactDOM.unmountComponentAtNode(document.getElementById('app'))
-      clearInterval(this.timer);
+    this.state ={
+      x:0,
+      y:0
     }
+    this.handleMouseMove = this.handleMouseMove.bind(this)
   }
-  componentWillUnmount(){
-    console.log("componentWillUnmount")
-    clearInterval(this.timer);
+  handleMouseMove(event){
+    //console.log(event.target)
+    //console.log(event.clientX)
+    this.setState({
+      x:event.clientX,
+      y:event.clientY
+    })
   }
-
   render(){
-    console.log("render")
     return (
-      <div>
-      <h1>hello</h1>
-      <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      <div onMouseMove={this.handleMouseMove} style={{
+        width:"1000px",
+        height:"700px"
+      }}>
+      X:{this.state.x}
+      <br />
+      Y:{this.state.y}
       </div>
     )
   }
 }
-
 
 
 ReactDOM.render(
