5b3855830ee5f3812e3b998def4a170cfafcada3
pengteling <pengteling@gmail.com>
2017-08-16 15:56:43 +0800

    ok

--- src/js/index.js
+++ src/js/index.js
@@ -3,57 +3,93 @@ import ReactDOM from 'react-dom';
 require("../sass/style.scss");
 import { CSSTransitionGroup } from 'react-transition-group' // ES6
 import { TransitionGroup } from 'react-transition-group' // ES6
+import TweenMax from 'gsap' //
 
-
-function ImageCarousel(props) {
-  return (
-    <div>
-      <CSSTransitionGroup
-        // transitionAppear={true}
-        // transitionAppearTimeout={1000}
-        transitionName="carousel"
-        transitionEnterTimeout={1000}
-        transitionLeaveTimeout={1000}>
-        <img src={props.imageSrc} key={props.imageSrc} />
-      </CSSTransitionGroup>
-    </div>
-  )
-}
-
-class Carousel extends React.Component{
+class Box extends React.Component{
   constructor(props){
     super(props)
-    this.state={
-      imageSrc:[
-        require("./../images/1.jpg"),
-        require("./../images/2.jpg"),
-        require("./../images/3.jpg"),
-        require("./../images/4.jpg"),
-      ],
-      currentIndex:0
-    }
+  }
+  componentWillAppear(callback){
+    console.log("componentWillAppear")
+    const el = ReactDOM.findDOMNode(this);
+		TweenMax.fromTo(el, 0.3, {x:300 ,y: 0, opacity: 0}, {x:0,y: 0, opacity: 1, onComplete: callback});
+    //callback();
+  }
+  componentDidAppear(){
+      console.log("componentDidAppear")
+      const el = ReactDOM.findDOMNode(this);
+		  TweenMax.fromTo(el, 0.3, {x:0 ,y: 0, opacity: 0}, {x:300,y: 100, opacity: 1 } );
   }
 
-  componentDidMount(){
-    this.timer = setInterval(
-      ()=>{
-        this.setState({currentIndex: (this.state.currentIndex + 1)% 4 })
-      }
-      ,3000)
+  componentWillEnter(callback){
+    console.log("componentWillEnter")
+    const el = ReactDOM.findDOMNode(this);
+        //callback();
+    TweenMax.fromTo(el, 0.3, {x:0 ,y: 100, opacity: 0}, {x:300,y: 0, opacity: 1, onComplete: callback});
+  }
+  componentDidEnter() {
+      	console.log("componentDidEnter")
   }
 
-
+  componentWillLeave (callback) {
+  	console.log("componentWillLeave");
+      const el = ReactDOM.findDOMNode(this);
+      TweenMax.fromTo(el, 0.3, {x:300 , y: 0, opacity: 1}, {x:0 ,y: -100, opacity: 0, onComplete: callback});
+  }
+  componentDidLeave() {
+  	   console.log('componentDidLeave');
+  }
+	componentDidMount() {
+		console.log("componentDidMount")
+	}
+  componentWillUnmount(){
+    console.log("componentWillUnmount")
+  }
   render(){
     return(
-      <div className="carousel">
-      <ImageCarousel imageSrc = {this.state.imageSrc[this.state.currentIndex]} />
+      <div className="box">
+            {/* style={{
+                width: '100px',
+                height: '100px',
+                margin: '100px',
+                borderRadius: '50%',
+                backgroundColor: 'red'
+            }}> */}
       </div>
     )
   }
 }
 
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      isshow:true
+    }
+    this.handleClick= this.handleClick.bind(this)
+  }
+  handleClick(){
+    this.setState({
+      isshow:!this.state.isshow
+    })
+  }
+  render(){
+    console.log("render")
+    return(
+      <div>
+        <input type="button" onClick={this.handleClick} value="点击切换"/>
+        <TransitionGroup>
+          {
+            //this.state.isshow?         <Box /> :""
+            this.state.isshow && <Box />               
+          }
+        </TransitionGroup>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <Carousel />,
+  <App />,
   document.getElementById('app')
 );
