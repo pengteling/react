dce5fbd067b158763bdfc89c0053b4a121bbfe5c
pengteling <pengteling@gmail.com>
2017-08-17 20:25:41 +0800

    eq immutability

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ItemList from './ItemList';
+import update from 'immutability-helper';
+
 export default class App extends React.PureComponent{
     constructor(props){
       super(props)
@@ -10,25 +12,30 @@ export default class App extends React.PureComponent{
       this.handleClick=this.handleClick.bind(this)
     }
     // shouldComponentUpdate(nextProps,nextState){
-    //   return true
+    //   console.log(this.olditems.length)
+    //   console.log(nextState.items.length)
+    //   if(nextState.items.length!== this.olditems.length){
+    //     return true
+    //   }
+    //   else{
+    //     return false
+    //   }
     // }
 
     handleClick(){
+      //this.olditems = this.state.items.slice()
       // let {items} = this.state
+      //
       // // console.log({items})
       // // console.log(items)
       // items.push("new-item")
       // //console.log({items})
       // this.setState({items})
 
-      // this.setState({
-      //   items: this.state.items.concat(['new-item']) //不会改变现有数组 是返回新的数组
-      // });
-
-      this.setState( (prevState)=>{
-        return {items:prevState.items.concat(['new-item'])}
-      });
-
+      //const newItems = update(this.state.items, {$push: ["new-item"]})
+      const newItems = update(this.state, {items: {$apply: x=>x} })
+      console.log(newItems)
+      this.setState({items:newItems.items})
     }
     render(){
       return(
@@ -40,5 +47,5 @@ export default class App extends React.PureComponent{
     }
 }
 App.defaultProps ={
-  
+
 }
