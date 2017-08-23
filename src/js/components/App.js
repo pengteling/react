21593dcf822c4ad46fbaa03d6ca9d642272f3d55
pengteling <pengteling@gmail.com>
2017-08-23 21:29:31 +0800

    not ok

--- src/js/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ShowCounter from "../containers/ShowCounter"
-
-const App = ()=>(
-  <div>
-    <ShowCounter />
-  </div>
-)
-export default App
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import doApp from './reducers'
-import App from './components/App'
+import ReactDOM from 'react-dom'
+import BasicExample from "./BasicExample"
 
-let store = createStore(doApp)
 
-console.log(store.getState())
-// store.subscribe(()=>console.log(store.getState()))
-// store.dispatch({type: "INCREASE"})
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+
+ReactDOM.render(
+  <AuthExample />,
   document.getElementById('root')
 )
