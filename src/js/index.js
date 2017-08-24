14bb283146763084adb9d7dbcd68d6c00eccc613
pengteling <pengteling@gmail.com>
2017-08-24 15:16:37 +0800

    eq ok not dong

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,10 @@
+import 'babel-polyfill'
+
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
+import Root from './containers/Root'
 
-let store = createStore(todoApp)
-
-console.log(store.getState())
-//store.subscribe(()=>console.log(store.getState()))
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root />,
   document.getElementById('root')
 )
