f51ae7997849cab123b2485c8d624205cf1c65f2
pengteling <pengteling@gmail.com>
2017-09-04 21:45:26 +0800

    lrc

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import ShowPlayer from '../containers/ShowPlayer'
 import ShowList from '../containers/ShowList'
-
+import Lrc from './Lrc'
 import {
   BrowserRouter as Router,
   Route
@@ -11,13 +11,17 @@ const App = ()=>(
   <Router>
     <div>
       <Header/>
-      
+
       <Route exact path ="/" render = {() =>(
         <ShowPlayer />
       )}>
       </Route>
       <Route path ="/list" render = {() =>(
         <ShowList />
+      )}>
+      </Route>
+      <Route path ="/lrc" render = {() =>(
+        <Lrc />
       )}>
       </Route>
 
---
+++ src/components/Lrc.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Lrc extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+
+    return(
+      <div>歌词</div>
+    )
+  }
+
+}
+export default Lrc
