50dbda3df874672935028c962ea28604c0f4117b
pengteling <pengteling@users.noreply.github.com>
2018-02-07 12:00:01 +0800

    Update store.js

--- api/store.js
+++ api/store.js
@@ -1,3 +1,5 @@
-{title:'JS高级进阶',quantity:5},
+[
+            {title:'JS高级进阶',quantity:5},
             {title:'Vuejs入门',quantity:2},
-            {title:'React精通与实务',quantity:1},
+            {title:'React精通与实务',quantity:1}
+ ]
